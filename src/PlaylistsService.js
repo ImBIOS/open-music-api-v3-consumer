5c50fdf5aa927f2c34a8ef99ad297f9b740f47f9
Imamuzzaki <imamuzzaki@gmail.com>
2021-07-04 09:46:13 +0700

    fix bug, exported all playlist instead one

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,13 +5,14 @@ class PlaylistsService {
     this._pool = new Pool();
   }
  
-  async getPlaylists(userId) {
+  async getPlaylists(userId, playlistId) {
     const query = {
       text: `SELECT playlists.* FROM playlists
       LEFT JOIN collaborations ON collaborations.playlist_id = playlists.id
-      WHERE playlists.owner = $1 OR collaborations.user_id = $1
+      WHERE (playlists.owner = $1 OR collaborations.user_id = $1)
+	    AND playlists.id = $2
       GROUP BY playlists.id`,
-      values: [userId],
+      values: [userId, playlistId],
     };
     const result = await this._pool.query(query);
     return result.rows;
